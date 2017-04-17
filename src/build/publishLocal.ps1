3e487f837785f62b10504ef50036a69a7934187f
1iveowl <liveowl@outlook.com>
2017-04-17 02:27:48 +0200

    csproj .NET Standard

--- src/build/publishLocal.ps1
+++ src/build/publishLocal.ps1
@@ -1,10 +1,10 @@
 param([string]$betaver)
 
 if ([string]::IsNullOrEmpty($betaver)) {
-	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\interface\ISocketLite.PCL\bin\Release\ISocketLite.PCL.dll')).Version.ToString(3)
+	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\interface\ISocketLite.Netstandard\bin\Release\netstandard1.0\ISocketLite.PCL.dll')).Version.ToString(3)
 	}
 else {
-	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\interface\ISocketLite.PCL\bin\Release\ISocketLite.PCL.dll')).Version.ToString(3) + "-" + $betaver
+	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\interface\ISocketLite.Netstandard\bin\Release\netstandard1.0\ISocketLite.PCL.dll')).Version.ToString(3) + "-" + $betaver
 }
 
 .\build.ps1 $version
