482984b53edbfff17e85465fcb702e85f9db0b3b
1iveowl <liveowl@outlook.com>
2017-04-17 02:02:12 +0200

    Move to VS 2017 & .NET Standard

---
+++ src/test/SocketLite.NETCore.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SocketLite.NETCore.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
